738de99840b73ce73f5e6435bcdc80c7311fd5cf
fql123 <fengqilong293@gmail.com>
2025-01-13 21:33:25 +0800

    debug

--- test/on_chip.py
+++ test/on_chip.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     target = LlamaForCausalLM.from_pretrained(args.target, torch_dtype=torch.float16, device_map="cuda:0")
     target = target.eval()
 
-    draft = LlamaForCausalLM.from_pretrained("NousResearch/Llama-3.2-1B", torch_dtype=torch.float16, device_map="cuda:0")
+    draft = LlamaForCausalLM.from_pretrained(args.target, torch_dtype=torch.float16, device_map="cuda:0")
     draft = draft.eval()
 
     tokenizer = AutoTokenizer.from_pretrained(args.target, use_fast=True, legacy=False)
