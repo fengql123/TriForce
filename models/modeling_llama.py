dfbc18d08e7b6e5cf67ac5485f9a0a2b45d0cccd
fql123 <fengqilong293@gmail.com>
2025-01-14 10:00:12 +0800

    debug

--- models/modeling_llama.py
+++ models/modeling_llama.py
@@ -228,9 +228,10 @@ class LlamaAttention(nn.Module):
         #     else:
         #         raise ValueError(f"Unknown RoPE scaling type {scaling_type}")
         self.rotary_emb = LlamaRotaryEmbedding(self.config)
-        scaling_type = self.config.rope_scaling["type"]
-        scaling_factor = self.config.rope_scaling["factor"]
-        if scaling_type == "yarn":
+        
+        if "type" in self.config.rope_scaling and self.config.rope_scaling["type"] == "yarn":
+            scaling_type = self.config.rope_scaling["type"]
+            scaling_factor = self.config.rope_scaling["factor"]
             original_max_position_embeddings = self.config.rope_scaling["original_max_position_embeddings"]
             self.rotary_emb = LlamaYaRNRotaryEmbedding(
                 self.head_dim, base=10000, scaling_factor=scaling_factor,
